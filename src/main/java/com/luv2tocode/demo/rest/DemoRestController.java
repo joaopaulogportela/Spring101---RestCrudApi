c8ae75d4a05aa3c04020be351d6e5508aea228f0
joaopauloportela <joaopauloportela@hotmail.com>
2024-09-12 15:55:56 +0100

    00- Demo for a rest controller with a get end-point "helloWorld. @RestController  to mark a class as an restController and to add an adress to in you use @RequestMapping("/addressName"). @GetMapping("/name") to build an get end-point.

---
+++ src/main/java/com/luv2tocode/demo/rest/DemoRestController.java
@@ -0,0 +1,14 @@
+package com.luv2tocode.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoRestController {
+    @GetMapping("/helloWorld")
+    public String helloWorld(){
+        return "Rellow World!";
+    }
+}
