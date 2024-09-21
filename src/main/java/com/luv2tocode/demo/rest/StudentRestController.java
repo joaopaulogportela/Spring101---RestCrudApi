c7952883df88c23e1aa219854bcf2216c8126f2d
joaopauloportela <joaopauloportela@hotmail.com>
2024-09-21 15:39:13 +0100

    02- (PathVariables - ResponseEntity) PathVariables go into the end-point. In get end-points you use path variables to send params to the server. The type ResponseEntity helps you send multiple types of data format as a RESPONSE.

--- src/main/java/com/luv2tocode/demo/rest/StudentRestController.java
+++ src/main/java/com/luv2tocode/demo/rest/StudentRestController.java
@@ -1,8 +1,11 @@
 package com.luv2tocode.demo.rest;
 
 import com.luv2tocode.demo.entity.Student;
-import org.springframework.boot.autoconfigure.batch.BatchTransactionManager;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +16,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudentList(){
-        List<Student> list = new ArrayList<>(List.of(
+    private List<Student> studentlist;
+    @PostConstruct
+    public void loadData(){
+        this.studentlist = new ArrayList<>(List.of(
                 new Student("111","Jonh","Paulo"),
                 new Student("222","Joao","Paul"),
                 new Student("333","Jonh","Paul")));
+    }
+    @GetMapping("/students")
+    public List<Student> getStudentList(){
+        return studentlist;
+    }
 
-        return list;
+    @GetMapping("/students/{studentId}")
+    public ResponseEntity<Object> getStudentById(@PathVariable String studentId){
+        Student student = studentlist.stream()
+                .filter(s -> s.getCode().equals(studentId))
+                .findFirst()
+                .orElse(null);
+
+        return (student == null)
+                ? ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Student with id " + studentId + " not found.")
+                : ResponseEntity.ok(student);
     }
 }
